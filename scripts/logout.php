dc726143c373cfa771d32856a54aa122236ecb26
Zemental <erick_sj_13@hotmail.com>
2016-06-18 13:09:23 -0500

    Revert "Iniciando proyecto"
    This reverts commit e0ccf4b1f865bf32dad1b8e85a58f73743953a83.

--- scripts/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_unset($_SESSION['id']);
-    session_unset($_SESSION['usuario']);
-    session_unset($_SESSION['nombres']);
-    session_unset($_SESSION['apellidos']);
-    session_destroy();
-    header("location:../index.php");
