9c337f07ac51b905f71d5a17b25b1d3fc3f423a3
ErickGzuck <erick.systems32@gmail.com>
2016-06-19 13:08:03 -0500

    mantenedor chips en construccion

---
+++ controllers/controlChip/chipController.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include_once '../../models/modelChip/chipModel.php';
+
+$param = array();
+$param['opcion'] = '';
+$param['codigo'] = '';
+$param['icc'] = '';
+$param['numero'] = '';
+$param['operadora'] = '';
+$param['estado'] = '';
+
+if (isset($_POST['opcion'])) {
+    $param['opcion'] = $_POST['opcion'];
+}
+
+if (isset($_POST['codigo'])) {
+    $param['codigo'] = $_POST['codigo'];
+}
+
+if (isset($_POST['icc'])) {
+    $param['icc'] = $_POST['icc'];
+}
+
+if (isset($_POST['numero'])) {
+    $param['numero'] = $_POST['numero'];
+}
+
+if (isset($_POST['operadora'])) {
+    $param['operadora'] = $_POST['operadora'];
+}
+
+if (isset($_POST['estado'])) {
+    $param['estado'] = $_POST['estado'];
+}
+
+$Chip = new ChipModel();
+echo $Chip->gestionar($param);
+
